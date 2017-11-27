ee5398c10780f25e489a0011975d013cb61831ce
Rafael Poveda <raerpo@gmail.com>
2017-11-26 23:14:19 -0300

    add top menu to main screen

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -3,13 +3,24 @@ import { withRouter } from 'react-router-dom';
 import { auth } from '../../config/firebase';
 import { HOME } from '../../constants/routes';
 
-const Main = withRouter(({ history }) => {
+const Main = withRouter(({ history, user }) => {
+  console.log(user);
   const onLogOutClick = () => {
     auth.signOut().then(() => {history.push(HOME)});
   }
-  return <div>
-    <h2>Main application</h2>
-    <button onClick={onLogOutClick}>Log out</button>
+  return <div className="ui menu">
+    <div className="item">
+      <h2>MDNotes</h2>
+    </div>
+    <div className="menu right">
+      <div className="item">
+        <img className="ui avatar image" src={user.photoURL} alt={user.displayName} />
+        <span>{ user.displayName }</span>
+      </div>
+      <div className="item">
+        <div className="ui button negative" onClick={onLogOutClick}>Log out</div>
+      </div>
+    </div>
   </div>
 });
  
