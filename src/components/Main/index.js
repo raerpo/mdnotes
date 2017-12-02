0ae5d293ecc40a76a5775c7100cd5be6429cf9f2
Rafael Poveda <raerpo@gmail.com>
2017-12-02 09:18:52 -0300

    update dependencies and todo for fragments support

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import TopMenu from '../TopMenu';
 
 const Main = ({ user, onLogOutClick }) => {
+  // TODO: Replace that wrapping div with a Fragment as 
+  // soon as CRA supports it
   return <div>
     <TopMenu user={user} onLogOutClick={onLogOutClick} />
   </div>
