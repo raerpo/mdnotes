dd24ed8a95ce009a4ae6fe864e4ddcf4eadb8748
Rafael Poveda <raerpo@gmail.com>
2017-12-09 10:01:32 -0300

    fix note writing

--- src/components/CodeEditor/index.js
+++ src/components/CodeEditor/index.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Controlled as CodeMirror } from 'react-codemirror2';
-// import debounce from 'lodash/debounce';
 
 import 'codemirror/lib/codemirror.css';
 import 'codemirror/mode/markdown/markdown';
@@ -14,30 +13,32 @@ class CodeEditor extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: ''
+      content: ''
     };
   }
   componentDidMount() {
     this.setState({
-      value: this.props.content
+      content: this.props.content
     });
   }
-  shouldComponentUpdate(nextProps) {
-    console.log('current: ', this.props);
-    console.log('next: ', nextProps);
-    return this.props.activeNote !== nextProps.activeNote;
+  componentWillReceiveProps(nextProps) {
+    if (this.props.content !== this.state.console) {
+      this.setState({
+        content: nextProps.content
+      })
+    }
   }
   handleChange = (e) => {
     const { changeNote, activeNote } = this.props;
-    changeNote(this.state.value, activeNote);
+    changeNote(this.state.content, activeNote);
   }
   render() {
     return (
       <CodeMirror
-        value={this.state.value}
+        value={this.state.content}
         options={options}
         onBeforeChange={(editor, data, value) => {
-          this.setState({ value }, () => {
+          this.setState({ content: value }, () => {
             this.handleChange();
           });
         }}
