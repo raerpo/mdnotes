3a8f06643dcae1e21c411217e5a89d1d990a20f0
Rafael Poveda <raerpo@gmail.com>
2017-12-11 21:41:12 -0300

    remove http content and change some styles

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,50 +1,45 @@
-import React from 'react';
-import cx from 'classnames';
+import React from "react";
+import cx from "classnames";
 
-import './index.css';
+import "./index.css";
 
 const Home = ({ onlogInWithGithub, isLogginIn }) => {
-  const buttonClassNames = cx('ui basic button massive main-wrapper_content_github', { loading: isLogginIn });
+  const buttonClassNames = cx(
+    "ui basic button massive main-wrapper_content_github",
+    { loading: isLogginIn }
+  );
   return (
     <div className="ui main-wrapper">
-
-      <div className="ui main-wrapper_background">
-      </div>
+      <div className="ui main-wrapper_background" />
 
       <div className="ui text main-wrapper_content">
-
         <div className="ui main-wrapper_menu">
-
           <img src="/MDnotes_logo-white.png" alt="" />
           <div className="ui main-wrapper_menu_content">
             <a href="/">Authors</a>
             <a href="/">Explore</a>
             <a href="/">Contact</a>
           </div>
-
         </div>
 
         <div className="ui main-wrapper_info">
-
           <h2>TAKE YOUR NOTES EVERYWHERE</h2>
-          <h3>Take notes wherever you are, even on your text editor, and keep them on sync all the time.</h3>
+          <h3>
+            Take notes wherever you are, even on your text editor, and keep them
+            on sync all the time.
+          </h3>
           <button className={buttonClassNames} onClick={onlogInWithGithub}>
-            <i className="icon github"></i>
+            <i className="icon github" />
             Login with Github
           </button>
-
         </div>
 
         <div className="ui main-wrapper_img">
-
-          <img src="http://via.placeholder.com/500x500" alt="" />
-
+          <img src="/500x500.png" alt="Temporal home" />
         </div>
-
       </div>
-
     </div>
   );
-}
+};
 
 export default Home;
