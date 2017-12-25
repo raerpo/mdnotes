daf9a3eb983405877551f721a532ee9025e4a6bb
Rafael Poveda <raerpo@gmail.com>
2017-12-25 16:53:03 -0300

    remove styled components dependency

--- src/constants/colors.js
+++
@@ -1,3 +0,0 @@
-export const backgroundColor = 'white';
-export const textColor = 'rgba(0, 0, 0, 0.8)';
-export const itemListActiveColor = '#eaeff5';
--- src/index.js
+++ src/index.js
@@ -1,20 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { backgroundColor, textColor } from './constants/colors';
-import { injectGlobal } from 'styled-components';
 import App from './App';
 
 import 'semantic-ui-css/semantic.min.css';
 import './custom.css';
-
-injectGlobal`
-  body {
-    margin: 0px;
-    font-size: 14px;
-    font-family: arial;
-    background-color: ${backgroundColor};
-    color: ${textColor}
-  }
-`;
 
 ReactDOM.render(<App />, document.getElementById('root'));
